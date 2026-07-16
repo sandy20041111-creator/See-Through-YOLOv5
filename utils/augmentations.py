1f6deb7571fed60d9cd60eea588c74c7b9487fc7
Yichen Lin <sandy20041111@gmail.com>
2026-07-17 02:30:17 +0800

    修正augmentations.py整數轉換問題

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -146,6 +146,7 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleF
         im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
     top, bottom = round(dh - 0.1), round(dh + 0.1)
     left, right = round(dw - 0.1), round(dw + 0.1)
+    top, bottom, left, right = int(top), int(bottom), int(left), int(right)
     im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
     return im, ratio, (dw, dh)
 
