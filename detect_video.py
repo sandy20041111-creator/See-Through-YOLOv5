38059ceaebfa08bad6bedffbe104f99ed23c7357
Yichen Lin <sandy20041111@gmail.com>
2026-07-17 19:05:54 +0800

    修正TensorRT模型跳過warmup

--- detect_video.py
+++ detect_video.py
@@ -240,7 +240,7 @@ def run(
     current_level = 0
     for path, im, im0s, vid_cap, s in dataset:
         with dt[0]:
-            im = torch.from_numpy(im).to(model.device)
+            im = torch.from_numpy(im).to(device)
             im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
             im /= 255  # 0 - 255 to 0.0 - 1.0
             if len(im.shape) == 3:
