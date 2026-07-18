e222b60ad61e036c8c3e068d5fd77c6b6b6c5b8e
Yichen Lin <sandy20041111@gmail.com>
2026-07-18 16:47:40 +0800

    修正TensorRT模式下的類別名稱

--- detect_video.py
+++ detect_video.py
@@ -215,6 +215,8 @@ def run(
         model.load_state_dict(torch.load(weights[0]))
         model = model.cuda().half()
         stride, names, pt = 32, [f'class{i}' for i in range(85)], False
+        stride, pt = 32, False
+        names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
         # 設定 TRTModule 缺少的屬性
         model.fp16 = True
         model.xml = False
